f49e58fddaf6a36d32b152189f36c251e211d74b
sai-manikanta44 <venkatasaimanikanta395@gmail.com>
2022-11-30 16:51:45 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+//import ClassComponent from './ClassComponent'
+import PropsExample from'./PropsExample'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <section>
+    <PropsExample name="Jenna"age="24"/>
+    <PropsExample name="Elena"age="20"/>
+      {/*<p>welcome to my world</p>
+      <h1>I am a Devil of my word</h1>
+      <img src="https://i.pinimg.com/474x/04/e1/78/04e1784fc85d72ccec586ca224ce361a.jpg" alt="image" width="15%"/>
+      <ClassComponent/> */}
+    </section>
+  )
 }
-
 export default App;
